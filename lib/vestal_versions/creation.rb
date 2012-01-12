de6b77213991045489321f005c8bff6b94cec13d
Sumeet Jain <sumeet@sumeetjain.com>
2012-01-11 22:53:16 -0800

    Store complete attributes for creation record.

--- lib/vestal_versions/creation.rb
+++ lib/vestal_versions/creation.rb
@@ -36,7 +36,7 @@ module VestalVersions
 
         # Creates an initial version upon creation of the parent record.
         def create_initial_version
-          versions.create(version_attributes.merge(:number => 1))
+					versions.create({:modifications => self.attributes, :number => 1})
           reset_version_changes
           reset_version
         end
